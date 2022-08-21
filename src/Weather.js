fbe2795cf65b32ddc0c72a799119da8b8e74e28e
Cjenae7 <104477982+Cjenae7@users.noreply.github.com>
2022-08-21 18:31:03 -0500

    edited css

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Weather.css";
 import "./App.css";
+import axios from "axios";
 export default function Weather() {
   return (
     <div className="container">
       <div className="weather-container">
-        <div className="Weather">
+        <div className="weather-app">
           <form id="search-form" class="mb-3">
             <div className="row">
               <div className="col-9">
@@ -51,8 +52,70 @@ export default function Weather() {
               <div class="wind">Wind: 10 mph</div>
             </div>
           </div>
+
+          <div class="row">
+            <div class="col-sm-2">
+              <div class="weather-forecast-date">Sat</div>
+              <img
+                src="http://openweather.org/img/wn/03d@2x.png"
+                alt
+                width="54"
+              ></img>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">92° </span>
+                <span class="weather-forecast-temperature-min">84° </span>
+              </div>
+            </div>
+            <div class="col-sm-2">
+              <div class="weather-forecast-date">Sun</div>
+              <img
+                src="http://openweather.org/img/wn/03d@2x.png"
+                alt
+                width="54"
+              ></img>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">89° </span>
+                <span class="weather-forecast-temperature-min">82° </span>
+              </div>
+            </div>
+            <div class="col-sm-2">
+              <div class="weather-forecast-date">Mon</div>
+              <img
+                src="http://openweather.org/img/wn/03d@2x.png"
+                alt
+                width="54"
+              ></img>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">90° </span>
+                <span class="weather-forecast-temperature-min">79° </span>
+              </div>
+            </div>
+            <div class="col-sm-2">
+              <div class="weather-forecast-date">Tues</div>
+              <img
+                src="http://openweather.org/img/wn/03d@2x.png"
+                alt
+                width="54"
+              ></img>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">86° </span>
+                <span class="weather-forecast-temperature-min">70° </span>
+              </div>
+            </div>
+            <div class="col-sm-2">
+              <div class="weather-forecast-date">Wed</div>
+              <img
+                src="http://openweather.org/img/wn/03d@2x.png"
+                alt
+                width="54"
+              ></img>
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">88° </span>
+                <span class="weather-forecast-temperature-min">78° </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="weather-forecast" id="forecast"></div>
       </div>
     </div>
   );
